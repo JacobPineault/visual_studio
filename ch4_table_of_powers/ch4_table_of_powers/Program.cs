6916a4f21c4ea04ee8a59460b850a9e9bd67a00f
jacobpineault <jacob.pineault5@gmail.com>
2020-04-15 12:03:23 -0400

    init commit

---
+++ ch4_table_of_powers/ch4_table_of_powers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ch4_table_of_powers {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Squares and Cubes table");
+
+            int number;
+            int squared;
+            int cubed;
+            string choice = "y";
+            int i = 1;
+
+            while (!choice.Contains("n")) { 
+            Console.WriteLine("Enter an integer: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Number  Squared  Cubed");
+                Console.WriteLine("======  =======  =====");
+
+
+                for (i = 1; i <= number; i++) {
+                    squared = i * i;
+                    cubed = i * i * i;
+
+                    Console.WriteLine(i + "\t" + squared + "  " + "\t" + cubed);
+
+                }
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+
+            }
+            Console.WriteLine("Bye");
+        }
+
+
+
+
+    }
+}
