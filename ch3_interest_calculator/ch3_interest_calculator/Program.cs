e6f64ed18175f2718dc24ffb2ec4cbb638080303
jacobpineault <jacob.pineault5@gmail.com>
2020-04-15 11:43:23 -0400

    init commit

---
+++ ch3_interest_calculator/ch3_interest_calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ch3_interest_calculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Interest Calculator");
+
+            double loanAmount;
+            double intRate;
+            double interest;
+            string choice = "y";
+
+            while (!choice.Contains("n")) {
+
+                Console.WriteLine("Enter loan amount: ");
+                loanAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter interest rate: ");
+                intRate = Convert.ToDouble(Console.ReadLine());
+
+                interest = (loanAmount * intRate);
+
+                Console.WriteLine(loanAmount.ToString("C"));
+                Console.WriteLine(intRate.ToString("P3"));
+                Console.WriteLine(interest.ToString("C"));
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye");
+
+        }
+    }
+}
