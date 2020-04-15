adde3e520b390809ec1411963c070b11e1cb3072
jacobpineault <jacob.pineault5@gmail.com>
2020-04-15 15:54:21 -0400

    init commit

---
+++ list-demo/list-demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_demo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("C# List Demo");
+
+
+            List<String> names = new List<string>();
+            names.Add("Trevor");
+            names.Add("Dennis");
+            names.Add("Eric");
+            names.Add("Jeff");
+
+            names.Remove("Dennis");
+
+
+            foreach (string s in names) {
+                Console.WriteLine(s);
+            }
+
+
+        }
+    }
+}
